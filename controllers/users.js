60cb0e0d4b447b6c4e79ea9a4a6c79e35dcbbc98
Jose Rosal <andresrosalaponteee@gmail.com>
2024-02-29 13:00:52 -0400

    guardamos con el save en la base de datos

--- controllers/users.js
+++ controllers/users.js
@@ -3,14 +3,20 @@ const usersRouter=require('express').Router();
 //router : registrar POST, GET,DELETE ,UPDATE
 const User = require("../model/user")
 //registar lo que el usuario me envia 
-usersRouter.post('/',(request,response)=>{
-    console.log(request,"req")
+usersRouter.post('/',async(request,response)=>{
+    //console.log(request,"req")
     console.log(request.body,"req.body");
     const {name,email,password}=request.body;
+    const body=request.body;
     console.log(name,email,password,"En el controlador");
 
     if(!name || !email || !password){
-            
+        try {
+            const userdb=new User(body);
+            await userdb.save();
+        } catch (error) {
+            console.log(error);
+        }
         return response.status(400).json({error:'Todos los campos son requeridos'})
     }else{
         
